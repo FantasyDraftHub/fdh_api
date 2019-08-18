309001dca5c9058671bfafa98bcd2265a24864ac
Brett Hammond <brett@hammond.io>
2019-08-18 01:38:01 -0600

    Added position to ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,7 +1,7 @@
 class RankingsController < ApplicationController
 
   def index
-    players = Player.includes(:position, :team).where.not(tier: nil)
+    players = Player.includes(:position, :team).joins(:position).where('positions.key = ?', params[:position]).where.not(tier: nil).order(:tier, :tier_index)
     render json: players.to_json({:include => [:position, :team], :methods => :name}), status: 200
   end
 end
